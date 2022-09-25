992d1523c5e70eb082f0c6186f106b599acb3bfa
Wolfero <ferorock44@gmail.com>
2022-09-25 15:55:18 +0100

    🧐 inspect image link bug

--- components/Banner/Banner.tsx
+++ components/Banner/Banner.tsx
@@ -19,21 +19,17 @@ const Banner = ({ netflixOriginals }: Props) => {
 		}
 	}, [netflixOriginals]);
 
-    console.log(movie)
+	console.log(movie);
 
 	return (
 		<div className="flex flex-col space-y-2 py-16 md:space-y-4 lg:h-[65vh] lg:justify-end lg:pb-12">
 			<div className="absolute top-0 left-0 -z-10 h-[95vh] w-screen">
-				{movie ? (
-					<Image
-						src={`${baseUrl}${movie?.backdrop_path || movie?.poster_path}`}
-						layout="fill"
-						objectFit="cover"
-						priority
-					/>
-				) : (
-					''
-				)}
+				<Image
+					src={`${baseUrl}${movie?.backdrop_path || movie?.poster_path}`}
+					layout="fill"
+					objectFit="cover"
+					priority
+				/>
 			</div>
 			<h1 className="text-2xl font-bold md:text-4xl lg:text-7xl">
 				{movie?.title || movie?.name || movie?.original_name}
