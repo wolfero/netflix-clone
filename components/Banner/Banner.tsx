49f28d54ad90de81bff66280575658516a0ed0a9
Wolfero <ferorock44@gmail.com>
2022-09-25 16:03:32 +0100

    🧐 inspect image link bug

--- components/Banner/Banner.tsx
+++ components/Banner/Banner.tsx
@@ -11,25 +11,26 @@ interface Props {
 
 const Banner = ({ netflixOriginals }: Props) => {
 	const [movie, setMovie] = useState<Movie | null>(null);
+	const [imageLink, setImageLink] = useState<string>('/#');
 
 	useEffect(() => {
 		if (netflixOriginals) {
 			const randomMovie = Math.floor(Math.random() * netflixOriginals.length);
 			setMovie(netflixOriginals[randomMovie]);
+			setImageLink(
+				baseUrl +
+					(netflixOriginals[randomMovie].backdrop_path ||
+						netflixOriginals[randomMovie].poster_path)
+			);
 		}
 	}, [netflixOriginals]);
 
-	console.log(movie);
+	console.log(imageLink);
 
 	return (
 		<div className="flex flex-col space-y-2 py-16 md:space-y-4 lg:h-[65vh] lg:justify-end lg:pb-12">
 			<div className="absolute top-0 left-0 -z-10 h-[95vh] w-screen">
-				<Image
-					src={`${baseUrl}${movie?.backdrop_path || movie?.poster_path}`}
-					layout="fill"
-					objectFit="cover"
-                    priority
-				/>
+				<Image src={imageLink} layout="fill" objectFit="cover" priority />
 			</div>
 			<h1 className="text-2xl font-bold md:text-4xl lg:text-7xl">
 				{movie?.title || movie?.name || movie?.original_name}
