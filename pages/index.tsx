f19ccfc3e5295ce3f281bae89ef4e7c347837fa2
Wolfero <ferorock44@gmail.com>
2022-09-25 20:55:16 +0100

    🧐 inspect server side fetch

--- pages/index.tsx
+++ pages/index.tsx
@@ -26,7 +26,7 @@ const Home = ({
 	topRated,
 	trendingNow,
 }: Props) => {
-	console.log(netflixOriginals,'index');
+	console.log(netflixOriginals,'index ');
 
 	return (
 		<div className="relative h-screen overflow-hidden bg-gradient-to-b from-gray-900/10 to-[#010511] lg:h-[140vh]">
