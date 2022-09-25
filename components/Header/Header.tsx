7f427894f0e38c89611d521eee2c52fc73eb6675
Wolfero <ferorock44@gmail.com>
2022-09-25 10:34:33 +0100

    ✨add scroll effect in header

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -1,9 +1,23 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { BellIcon, MagnifyingGlassIcon as SearchIcon } from '@heroicons/react/24/solid';
+import Link from 'next/link';
 
 function Header() {
+	const [isScrolled, seTisScrolled] = useState(false);
+	useEffect(() => {
+		const handleScroll = () => {
+			const windowScroll = window.scrollY > 0;
+			windowScroll ? seTisScrolled(true) : seTisScrolled(false);
+		};
+		window.addEventListener('scroll', handleScroll);
+
+		return () => {
+			window.removeEventListener('scroll', handleScroll);
+		};
+	}, []);
+
 	return (
-		<header>
+		<header className={`${isScrolled && 'bg-[#141414]'}`}>
 			<div className="flex items-center space-x-2 md:space-x-10">
 				<img
 					src="/assets/NetflixLogo.svg"
@@ -19,10 +33,17 @@ function Header() {
 					<li className="headerLink">My List</li>
 				</ul>
 			</div>
-			<div>
+			<div className="flex items-center space-x-4 text-sm font-light">
 				<SearchIcon className="hidden h-6 w-6 sm:inline" />
 				<p className="hidden lg:inline">Kids</p>
 				<BellIcon className="h-6 w-6" />
+				<Link href="/account">
+					<img
+						src="/assets/accountLogo.png"
+						alt="Account Logo"
+						className="cursor-pointer rounded"
+					/>
+				</Link>
 			</div>
 		</header>
 	);
