825195d16096a01ecd961f12a7780f0ab0d7f6cf
Wolfero <ferorock44@gmail.com>
2022-09-25 10:49:45 +0100

    ✨add Banner component

---
+++ components/Banner/Banner.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Banner = () => {
+	return (
+		<div>
+			{/* <div>
+				<Image />
+			</div> */}
+		</div>
+	);
+};
+
+export default Banner;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,19 +1,21 @@
 import type { NextPage } from 'next';
 import Head from 'next/head';
+
+import Banner from '../components/Banner/Banner';
 import Header from '../components/Header/Header';
 
 const Home: NextPage = () => {
 	return (
-		<div>
+		<div className="relative h-screen bg-gradient-to-b from-gray-900/10 to-[#010511] lg:h-[140vh]">
 			<Head>
 				<title>Home - Netflix Clone By Fero</title>
 				<link rel="icon" href="/assets/netflix.ico" />
 			</Head>
 
-      <Header/>
-			
-      <main>
-				{/* Banner */}
+			<Header />
+
+			<main>
+				<Banner />
 				<section>
 					{/* Row */}
 					{/* Row */}
