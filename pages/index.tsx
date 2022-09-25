15ba3aef2339aded9d37b6472b1b5f6aaa726765
Wolfero <ferorock44@gmail.com>
2022-09-25 20:58:54 +0100

    🧐 inspect load banner img

--- pages/index.tsx
+++ pages/index.tsx
@@ -26,8 +26,6 @@ const Home = ({
 	topRated,
 	trendingNow,
 }: Props) => {
-	console.log(netflixOriginals,'index ');
-
 	return (
 		<div className="relative h-screen overflow-hidden bg-gradient-to-b from-gray-900/10 to-[#010511] lg:h-[140vh]">
 			<Head>
