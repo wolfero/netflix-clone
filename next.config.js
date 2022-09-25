06d3f7ccef39cb89e7c685855ecc51d1985f9565
Wolfero <ferorock44@gmail.com>
2022-09-25 15:13:09 +0100

    🐛 fix next.config

--- next.config.js
+++ next.config.js
@@ -1,9 +1,8 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
-  basePath: 'wolfero-netflix-clone.vercel.app',
   images: {
     domains: ['image.tmdb.org'],
-    path: `${basePath}/_next/image`,
+    path: `/_next/image`,
   }
 }
