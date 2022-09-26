5dc3955111722d4ab5b836e1f46729aedf4324ce
Wolfero <ferorock44@gmail.com>
2022-09-26 14:05:12 +0100

    ✨ added row component

---
+++ components/Row/Row.tsx
@@ -0,0 +1,32 @@
+import { ChevronLeftIcon, ChevronRightIcon } from '@heroicons/react/24/outline';
+import React from 'react';
+import { Movie } from '../../typings';
+import Thumbnail from '../Thumbnail/Thumbnail';
+
+interface Props {
+	movies: Movie[];
+	title: string;
+}
+
+const Row = ({ movies, title }: Props) => {
+	return (
+		<div className="relative h-40 space-y-0.5 pl-4 md:space-y-1 lg:pl-16">
+			<h2 className="w-56 cursor-pointer text-sm font-semibold text-[#e5e5e5] transition duration-200 hover:text-white md:text-2xl">
+				{title}
+			</h2>
+			<div className="group relative md:-ml-2">
+				<ChevronLeftIcon className="thumbnailButton left-2" />
+
+				<div className="flex items-center space-x-1 overflow-x-scroll scrollbar-hide md:ml-2 md:space-x-2.5">
+					{movies.map((movie) => (
+						<Thumbnail key={movie.id} movie={movie} />
+					))}
+				</div>
+
+				<ChevronRightIcon className="thumbnailButton right-2" />
+			</div>
+		</div>
+	);
+};
+
+export default Row;
