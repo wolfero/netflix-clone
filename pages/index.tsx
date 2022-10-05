6310e71eb50a636b427b9f2d15081df3c2a97aaf
Wolfero <ferorock44@gmail.com>
2022-10-05 13:22:03 +0100

    💄 fix scroll in page

--- pages/index.tsx
+++ pages/index.tsx
@@ -31,7 +31,7 @@ const Home = ({
 }: Props) => {
 	
 	return (
-		<div className="relative h-screen overflow-hidden bg-gradient-to-b from-gray-900/10 to-[#010511] lg:h-[140vh]">
+		<div className="relative h-screen bg-gradient-to-b from-gray-900/10 to-[#010511] lg:h-[140vh]">
 			<Head>
 				<title>Home - Netflix Clone By Fero</title>
 				<link rel="icon" href="/assets/netflix.ico" />
